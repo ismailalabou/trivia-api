2baf70faa165d9481debb5d6369913e7d76efeb8
Ismail ALABOU <alabou.ismail@gmail.com>
2020-12-30 20:17:57 +0100

    revert

--- GetAllQuestions/index.js
+++ GetAllQuestions/index.js
@@ -4,13 +4,6 @@ module.exports = async function (context, req) {
   context.log("Request for all questions");
 
   context.res = {
-    body: questions.map(function (question) {
-    return {
-      question: question.question,
-      answers: question.incorrect_answers
-        .concat([question.correct_answers])
-        .sort()
-    };
-    })
+    body: questions
   };
 };
